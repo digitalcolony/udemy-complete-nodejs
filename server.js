0f58ea1a93610d4195fbba0484266cbd7b983f04
MAS <digitalcolony@gmail.com>
2018-01-24 18:43:44 -0800

    Setup Start Script and Heroku PORT

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
@@ -19,11 +21,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render("maintenance", {
-    pageTitle: "Site Down"
-  });
-});
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs", {
+//     pageTitle: "Site Down"
+//   });
+// });
 
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
@@ -52,6 +54,6 @@ app.get("/bad", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on Port 3000.");
+app.listen(port, () => {
+  console.log(`Server is up on Port ${port}.`);
 });
